e7d39b8184bc915166ddadf09b646bffdf67c6d9
Garrett Yamano <gyama009@wch136-25.cs.ucr.edu>
2020-04-16 22:02:03 -0700

    Completed part 2

--- turnin/gyama009_lab4_part2.c
+++ turnin/gyama009_lab4_part2.c
@@ -25,11 +25,11 @@ void Tick(){
 				state = Reset;
 				PORTC = 0x00;
 			}
-			else if((PINA & 0x02)){
+			else if(PINA & 0x02){
 				state = Decrement;
 				PORTC -= 1;
 			}
-			else if((PINA & 0x01)){
+			else if(PINA & 0x01){
 				state = Increment;
 				PORTC += 1;
 			}
@@ -62,15 +62,15 @@ void Tick(){
 			}
 			break;
 		case Reset:
-			if((PINA & 0x03)){
+			if(PINA & 0x03){
 				state = Reset;
 				PORTC = 0x00;
 			}
-			else if((PINA & 0x01)){
+			else if(PINA & 0x01){
 				state = Increment;
 				PORTC += 1;
 			}
-			else if((PINA & 0x02)){
+			else if(PINA & 0x02){
 				state = Decrement;
 				PORTC -= 1;
 			}
