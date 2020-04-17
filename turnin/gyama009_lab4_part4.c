6cd039dcfcc008e79e1594214186f64ede9d26f9
Garrett Yamano <gyama009@wch136-25.cs.ucr.edu>
2020-04-17 11:43:05 -0700

    Completed Part 4

--- turnin/gyama009_lab4_part4.c
+++ turnin/gyama009_lab4_part4.c
@@ -14,10 +14,13 @@
 
 enum States{Start, Lock, On_Release, Open} state;
 
+unsigned char unlock;
+
 void Tick(){
 	switch(state){
 		case Start: // Initial transition
 			state = Lock;
+			unlock = 0;
 			break;
 		case Lock:
 			if((PINA & 0x04) == 0x04){
@@ -29,8 +32,15 @@ void Tick(){
 			break;
 		case On_Release:
 			if((PINA & 0x02) == 0x02){
-				state = Open;
-				PORTB = 0x01;
+				if(unlock == 0){
+					state = Open;
+					PORTB = 0x01;
+					unlock = 1;
+				}
+				else{
+					state = Lock;
+					unlock = 0;
+				}
 			}
 			else if((PINA & 0x00) == 0x00){
 				state = On_Release;
@@ -42,6 +52,9 @@ void Tick(){
 		case Open:
 			if((PINA & 0x80) == 0x80){
 				state = Lock;
+			}
+			else if((PINA & 0x04) == 0x04){
+				state = On_Release;
 			}
 			else{
 				state = Open;
